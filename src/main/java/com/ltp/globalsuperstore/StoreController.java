348e71d7ee389847cd659df7fadbd3b73449d2cf
Daniel Isangedighi <danielisangedighi@outlook.com>
2024-05-14 12:46:18 +0100

    I complete implemenation of cross-field validation The submitHandler method Validates by comparing price to discount.

--- src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -24,14 +24,16 @@ public class StoreController {
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getIndexFromId(id);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
+        //model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 
     @PostMapping("/submitItem")
     public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
-        if (result.hasErrors()) return "form";
-            
+        if (item.getPrice() < item.getDiscount()) {
+            result.rejectValue("price", "", "Price cannot be lower than the discount");
+        }
+        if (result.hasErrors()) return "form";   
         int index = getIndexFromId(item.getId());
         String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
